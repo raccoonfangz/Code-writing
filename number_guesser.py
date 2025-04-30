791847041cb052fe97bf53113be325a3e1e28ab7
Ash <adam.a.pacelt@gmail.com>
2025-04-30 19:30:20 +0200

    Update number_guesser.py
    added difficulty, guess count

--- number_guesser.py
+++ number_guesser.py
@@ -1,21 +1,25 @@
 import random
+import sys
 
 
 difficulty = input(
     "Select your difficulty: \n1. Easy (1-10) \n2. Medium (1-50)\n3. Hard (1-100)\n")
 
-if difficulty == 1 or 'easy':
+if difficulty == '1' or difficulty.lower() == 'easy':
     random_number = random.randint(1, 10)
-elif difficulty == 2 or 'medium':
+    print("Selected mode: Easy \n")
+elif difficulty == '2' or difficulty.lower() == 'medium':
     random_number = random.randint(1, 50)
-elif difficulty == 3 or "hard":
+    print("Selected mode: Medium \n")
+elif difficulty == '3' or difficulty.lower() == "hard":
     random_number = random.randint(1, 100)
+    print("Selected mode: Hard \n")
 else:
     print("Error: Wrong difficulty selection input")
-    exit()
+    sys.exit()
 
 
-print("Start guessing 1-10")
+print("Start guessing!")
 
 guess_count = 0
 
